6378b070e27a5dbddb2ba68e907fac3b918451d0
kshit0708e <chauhanparikshit7@gmail.comm>
2022-06-19 16:13:18 +0530

    2nd question

--- kshit.js
+++ kshit.js
@@ -1,11 +1,35 @@
 
+// ----------answer1-------------
 
+// let num=parseInt(process.argv[2])
 
-let num=parseInt(process.argv[2])
+// for(let i=1; i<=num ;i++){
+//     if(i%2!=0){
+//         console.log(i);
+//     }
+//     }
 
-for(let i=1; i<=num ;i++){
-    if(i%2!=0){
-        console.log(i);
-    }
-    }
-       
+// ----------answer2-------------
+
+// let num1=parseInt(process.argv[2])
+// let num2=parseInt(process.argv[3])
+// for (let i = num1; i < num2; i++) {
+// if (i%2==0) {
+    
+//     console.log(i++);    
+// }    
+// }
+
+// ----------answer3-------------
+
+let num = parseInt(process.argv[2]);
+let sum = 0;
+let firstdigit;
+let lastdigit;
+
+lastdigit = num % 10;
+while (num >= 10) {
+    num = Math.floor(num / 100);
+}
+firstdigit = num;
+console.log(firstdigit, lastdigit);
